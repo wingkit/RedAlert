f8dd97968abdc52b0d261aa0956cdae10f8fe343
Wingkit <525424725@qq.com>
2016-06-28 15:19:26 +0800

    实现同步网络模型 1、测量网络传输时间 2、发送命令

--- Js/multiplayer.js
+++ Js/multiplayer.js
@@ -2,6 +2,7 @@
 	// 打开多人对战大厅
 	websocket_url: "ws://localhost:8090/",
 	websocket: undefined,
+	chrrentLevel:0,
 	start: function () {
 		game.type = "multiplayer";
 		var WebSocketObject = window.WebSocket || window.MozWebSocket;
@@ -10,6 +11,7 @@
 			return;
 		}
 		this.websocket = new WebSocketObject(this.websocket_url);
+		// 当收到信息时触发
 		this.websocket.onmessage = multiplayer.handleWebSocketMessage;
 		// 连接后显示多人对战大厅界面
 		this.websocket.onopen = function () {
@@ -29,9 +31,23 @@
 				multiplayer.roomId = messageObject.roomId;
 				multiplayer.color = messageObject.color;
 				break;
+			case "init_level":
+				multiplayer.initMultiplayerLevel(messageObject);
+				break;
+			case "start_game":
+				multiplayer.startGame();
+				break;
+			case "latency_ping":
+				multiplayer.sendWebSocketMessage({ type: "latency_pong" });
+				break;
+			case "game_tick":
+				multiplayer.lastReceivedTick = messageObject.tick;
+				multiplayer.commands[messageObject.tick] = messageObject.commands;
+				break;
 		}
 
 	},
+	// 状态信息
 	statusMessage: {
 		'starting': 'Game Starting',
 		'running': 'Game in Progress',
@@ -92,4 +108,116 @@
 	sendWebSocketMessage: function (messageObject) {
 		this.websocket.send(JSON.stringify(messageObject));
 	},
+	initMultiplayerLevel: function (messageObject) {
+		$('.gamelayer').hide();
+		var spawnLocations = messageObject.spawnLocations;
+
+		// 初始化U盾偶人对战相关的变量
+		multiplayer.commands = [[]];
+		multiplayer.lastReceivedTick = 0;
+		multiplayer.currentTick = 0;
+
+		game.team = multiplayer.color;
+
+		// 为关卡加载所有的单位
+		multiplayer.currentLevel = messageObject.level;
+		var level = maps.multiplayer[multiplayer.currentLevel];
+
+		// 关卡加载所有的资源
+		game.currentMapImage = loader.loadImage(level.mapImage);
+		game.currentLevel = level;
+
+		// 基于来自服务器的出生位置设置偏移量
+		// 加载关卡所需要的单位类型
+		game.resetArrays();
+		for (var type in level.requirements) {
+			var requirementArray = level.requirements[type];
+			for (var i = 0; i < requirementArray.length; i++) {
+				var name = requirementArray[i];
+				if (window[type]) {
+					window[type].load(name);
+				} else {
+					console.log("加载失败：" + type);
+				}
+			};
+		};
+
+		for (var i = level.items.length - 1 ; i >= 0; i--) {
+			var itemDetails = level.items[i];
+			game.add(itemDetails);
+		};
+
+		// 在玩家的出生位置分别为其添加起始单位
+		for (team in spawnLocations) {
+			var spawnIndex = spawnLocations[team];
+			for (var i = 0; i < level.teamStartingItems.length; i++) {
+				var itemDetails = $.extend(true, {}, level.teamStartingItems[i]);
+				itemDetails.x += level.spawnLocations[spawnIndex].x + itemDetails.x;
+				itemDetails.y += level.spawnLocations[spawnIndex].y + itemDetails.y;
+				itemDetails.team = team;
+				game.add(itemDetails);
+			};
+
+			if (team == game.team) {
+				game.offsetX = level.spawnLocations[spawnIndex].startX * game.gridSize;
+				game.offsetY = level.spawnLocations[spawnIndex].startY * game.gridSize;
+			}
+		};
+
+		// 创建可通行网格，可通行的网格值为1，不可通行的网格值为0
+		game.currentMapTerrainGrid = [];
+		for (var y = 0; y < level.mapGridHeight; y++) {
+			game.currentMapTerrainGrid[y] = [];
+			for (var x = 0; x < level.mapGridWidth; x++) {
+				game.currentMapTerrainGrid[y][x] = 0;
+			};
+		};
+		for (var i = level.mapObstructedTerrain.length - 1; i >= 0; i--) {
+			var obstruction = level.mapObstructedTerrain[i];
+			game.currentMapTerrainGrid[obstruction[1]][obstruction[0]] = 1;
+		};
+		game.currentMapPassableGrid = undefined;
+
+		// 为游戏加载启动资金
+		game.cash = $.extend([], level.cash);
+
+		// 加载资源完成后，启用“开始任务”按钮
+		if (loader.loaded) {
+			multiplayer.sendWebSocketMessage({ type: "initialized_level" });
+		} else {
+			loader.onload = function(){
+				multiplayer.sendWebSocketMessage({ type: "initialized_level" });
+			}
+		}
+	},
+	startGame: function () {
+		fog.initLevel();
+		game.animationLoop();
+		multiplayer.animationInterval = setInterval(multiplayer.tickLoop, game.animationTimeout);
+		game.start();
+	},
+	sendCommand: function (uids, details) {
+		multiplayer.sentCommandForTick = true;
+		multiplayer.sendWebSocketMessage({ type: "command", uids: uids, details: details, currentTick: multiplayer.currentTick });
+
+	},
+	tickLoop: function () {
+		// 如果该步进数的命令被接收了，就执行命令并进行下一次步进，否则，等待服务器捕获
+		if (multiplayer.currentTick <= multiplayer.lastReceivedTick) {
+			var commands = multiplayer.commands[multiplayer.currentTick];
+			if (commands) {
+				for (var i = 0; i < commands.length; i++) {
+					game.processCommand(commands[i].uids, commands[i].details);
+				};
+			}
+			game.animationLoop();
+
+			// 当前步进时没有命令，则向服务器发送空命令，这样服务器就知道客户端在流畅地运行
+			if (!multiplayer.sentCommandForTick) {
+				multiplayer.sendCommand();
+			}
+			multiplayer.currentTick++;
+			multiplayer.sentCommandForTick = false;
+		}
+	}
 };
--- Js/server.js
+++ Js/server.js
@@ -21,13 +21,7 @@ function connectionIsAllowed(request) {
 	// 检查条件， 如request.origin 和 request.remoteAddress
 	return true;
 }
-
-// 初始化一组房间
-var gameRooms = [];
-for (var i = 0; i < 10; i++) {
-	gameRooms.push({ status: "empty", players: [], roomId: i + 1 });
-};
-
+// 当前在线玩家
 var players = [];
 wsServer.on('request', function (request) {
 	if (!connectionIsAllowed(request)) {
@@ -42,11 +36,15 @@ wsServer.on('request', function (request) {
 	// 将player 加入到 players数组中
 	var player = {
 		connection: connection,
+		latencyTrips: [],
 	};
 	players.push(player);
 
 	// 玩家首次连接时，发送一个空的游戏房间状态列表
 	sendRoomList(connection);
+
+	// 为玩家测量传输延迟
+	measureLatency(player);
 
 	// 连接的onmessgae事件响应函数
 	connection.on('message', function (message) {
@@ -55,12 +53,36 @@ wsServer.on('request', function (request) {
 			switch (clientMessage.type) {
 				// 处理不同类型的消息
 				case "join_room":
-					var clientMessage = JSON.parse(message.utf8Data);
+					var room = joinRoom(player, clientMessage.roomId);
 					sendRoomListToEveryone();
+					if (room.players.length == 2) {
+						initGame(room);
+					}
 					break;
 				case "leave_room":
 					leaveRoom(player, clientMessage.roomId);
 					sendRoomListToEveryone();
+					break;
+				case "initialized_level":
+					player.room.playersReady++;
+					if (player.room.playersReady == 2) {
+						startGame(player.room);
+					}
+					break;
+				case "latency_pong":
+					finishMeasuringLatency(player, clientMessage);
+					// 至少三次侧脸传输延迟
+					if (player.latencyTrips.length < 3) {
+						measureLatency(player);
+					}
+					break;
+				case "command":
+					if (player.room && player.room.status == "running") {
+						if (clientMessage.uids) {
+							player.room.commands.push({ uids: clientMessage.uids, details: clientMessage.details });
+						}
+						player.room.lastTickConfirmed[player.color] = clientMessage.currentTick + player.tickLag;
+					}
 					break;
 			}
 		}
@@ -83,6 +105,12 @@ wsServer.on('request', function (request) {
 		}
 	});
 });
+
+// 初始化一组房间
+var gameRooms = [];
+for (var i = 0; i < 10; i++) {
+	gameRooms.push({ status: "empty", players: [], roomId: i + 1 });
+};
 
 function sendRoomList(connection) {
 	var status = [];
@@ -116,7 +144,7 @@ function joinRoom(player, roomId) {
 	if (room.players.length == 1) {
 		room.status = "waiting";
 		player.color = "blue";
-	} else if(room.players.length ==2) {
+	} else if (room.players.length == 2) {
 		room.status = "starting";
 		player.color = "green";
 	}
@@ -128,7 +156,7 @@ function joinRoom(player, roomId) {
 
 function leaveRoom(player, roomId) {
 	var room = gameRooms[roomId - 1];
-	console.log("Removing player from room", rooomId);
+	console.log("Removing player from room", roomId);
 
 	for (var i = room.players.length - 1; i >= 0; i--) {
 		if (room.players[i] == player) {
@@ -142,4 +170,90 @@ function leaveRoom(player, roomId) {
 	} else if (room.players.length == 1) {
 		room.status = "waiting";
 	}
+}
+
+function initGame(room) {
+	console.log(room.roomId + "号房间正在初始化游戏");
+
+	// 已加载关卡的玩家数量
+	room.playersReady = 0;
+
+	// 为两个玩家加载多人对战关卡，这里可以做改变，以云溪玩家选择一个关卡
+	var currentLevel = 0;
+
+	// 为两位玩家随机出生点（0~3之间）
+	var spawns = [0, 1, 2, 3];
+	var spawnLocations = {
+		"blue": spawns.splice(Math.floor(Math.random() * spawns.length), 1),
+		"green": spawns.splice(Math.floor(Math.random() * spawns.length), 1),
+	};
+	sendRoomWebSocketMessage(room, { type: "init_level", spawnLocations: spawnLocations, level: currentLevel });
+
+}
+
+function startGame(room) {
+	console.log(room.roomId + "号房间的玩家已经准备就绪。");
+	room.status = "running";
+	sendRoomListToEveryone();
+	// 通知玩家开始游戏
+	sendRoomWebSocketMessage(room, { type: "start_game" });
+
+	room.commands = [];
+	room.lastTickConfirmed = { "blue": 0, "green": 0 };
+	room.currentTick = 0;
+
+	// 房间的步进延迟为两个玩家各自步进延迟中的较大者
+	var roomTickLag = Math.max(room.players[0].tickLag, room.players[1].tickLag);
+
+	room.interval = setInterval(function () {
+		// 确认到步进时，两个玩家都已经发送过了命令
+		if (room.lastTickConfirmed["blue"] >= room.currentTick
+			&& room.lastTickConfirmed["green"] >= room.currentTick) {
+			// 命令将在步进延迟后执行
+			sendRoomWebSocketMessage(room, { type: "game_tick", tick: room.currentTick + roomTickLag, commands: room.commands });
+			room.currentTick++;
+			room.commands = [];
+		} else {
+			// 一个玩家导致游戏延迟，则做相应的处理
+			if (room.lastTickConfirmed["blue"] < room.currentTick - 1) {
+				console.log("Room", room.roomId, "Blue is lagging on Tick:",
+					room.currentTick, "by", room.currentTick - room.lastTickConfirmed["blue"]);
+			}
+			if (room.lastTickConfirmed["green"] < room.currentTick - 1) {
+				console.log("Room", room.roomId, "Green is lagging on Tick:",
+					room.currentTick, "by", room.currentTick - room.lastTickConfirmed["green"]);
+			}
+		}
+	}, 100);
+}
+
+function sendRoomWebSocketMessage(room, messageObject) {
+	var messageString = JSON.stringify(messageObject);
+	for (var i = room.players.length - 1; i >= 0; i--) {
+		room.players[i].connection.send(messageString);
+	};
+}
+
+// 测量网络传输时间的方法
+function measureLatency(player) {
+	var connection = player.connection;
+	var measurement = { start: Date.now() };
+	player.latencyTrips.push(measurement);
+	var clientMessage = { type: "latency_ping" };
+	connection.send(JSON.stringify(clientMessage));
+}
+
+function finishMeasuringLatency(player, clientMessage) {
+	var measurement = player.latencyTrips[player.latencyTrips.length - 1];
+	measurement.end = Date.now();
+	measurement.roundTrip = measurement.end - measurement.start;
+	player.averageLatency = 0;
+	for (var i = 0; i < player.latencyTrips.length; i++) {
+		player.averageLatency += measurement.roundTrip / 2;
+	};
+	player.averageLatency = player.averageLatency / player.latencyTrips.length;
+	player.tickLag = Math.round(player.averageLatency * 2 / 100) + 1;
+	console.log("Measuring Latency for player.Attempt ",
+		player.latencyTrips.length, "- Average Latency:",
+		player.averageLatency, "Tick Lag:", player.tickLag);
 }
